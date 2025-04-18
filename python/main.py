d68a15a216b38e5090d7a803117228ce6b7974a7
Ryan Evanyo <revanyo@vertafore.com>
2025-04-18 12:03:57 -0600

    refactor csv func

--- python/main.py
+++ python/main.py
@@ -1,6 +1,8 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+import os
 import requests
 import pandas as pd
+from utils.utils import get_csv_data, save_csv_data
 
 
 def get_current_price(pair):
@@ -13,19 +15,19 @@ def get_current_price(pair):
     return float(last_price)
 
 def get_prices():
-    pairs = pd.read_csv("../pairs.csv", names=["coin","pair"])
+    pairs = get_csv_data("pairs")
     
     new_data = {
     }
 
-    df = pd.read_csv("../historic_data.csv", index_col=0)
+    df = get_csv_data("historic_data", index_col=0)
 
     today = datetime.now().strftime('%Y-%m-%d')
     df[today] = float(0)  # Set default value as 0 for today's prices
 
     for index, row in pairs.iterrows():
-        coin= row["coin"]
-        pair = row["pair"]
+        coin= row["Coin"]
+        pair = row["Pair"]
         new_data[coin] = get_current_price(pair)
 
         if coin not in df.index:
@@ -35,6 +37,27 @@ def get_prices():
         df.at[coin, today] = price
 
 
-    df.to_csv("../historic_data.csv")
+    save_csv_data(df, "historic_data")
 
+def analyze_data():
+     df = get_csv_data("historic_data")
+     df.columns = pd.to_datetime(df.columns)
+     latest_date = df.columns.max()
+     one_week_ago = latest_date - timedelta(days=7)
 
+     for coin in df.index:
+        current = df.at[coin, latest_date]
+        past = df.at[coin, one_week_ago]
+
+        try:
+            current = float(current)
+            past = float(past)
+
+            change = current - past
+            pct_change = (change / past) * 100 if past != 0 else float('inf')
+
+            print(f"{coin}: {past} ➝ {current} | Change: {change:.2f} ({pct_change:.2f}%)")
+        except:
+            print(f"{coin}: Could not compare (missing or invalid data)")
+
+get_prices()
